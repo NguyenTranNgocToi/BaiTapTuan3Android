558007fdd40e2c09eac5e55f2b955152a6270317
Tuan3TrenLop <nguyentoi53@gmail.com>
2021-03-29 11:35:17 +0700

    tuan 3 tren lop

---
+++ app/src/main/java/com/example/baitaptuan3/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.baitaptuan3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        Button btnChonMau;
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.scr1);
+        
+    }
+
+
+}
