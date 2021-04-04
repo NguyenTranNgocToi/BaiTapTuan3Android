28669628cc41e4e715a7c95580a47f404ef10423
Nguyễn Trần Ngọc Tới <nguyentoi53@gmail.com>
2021-04-04 22:28:15 +0700

    xong gui

--- app/src/main/java/com/example/baitaptuan3/MainActivity.java
+++ app/src/main/java/com/example/baitaptuan3/MainActivity.java
@@ -12,8 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         Button btnChonMau;
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.scr1);
-        
+        setContentView(R.layout.scr3);
     }
 
 
